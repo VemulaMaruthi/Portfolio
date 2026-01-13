945973611214194b096f59bbd8b46324b11d4498
VemulaMaruthi <maruthivemula95@gmail.com>
2026-01-13 13:27:14 +0530

    updated the email

--- Portfolio_Backend/Portfolio/src/main/java/com/Maruthi/Portfolio/PortfolioApplication.java
+++ Portfolio_Backend/Portfolio/src/main/java/com/Maruthi/Portfolio/PortfolioApplication.java
@@ -2,8 +2,9 @@ package com.Maruthi.Portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mail.autoconfigure.MailSenderAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MailSenderAutoConfiguration.class)
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
